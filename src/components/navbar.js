500d8f8c61682f02fb00380780fbbcfe405494f1
ZiadEzzat133 <76906793+ZiadEzzat133@users.noreply.github.com>
2023-08-15 14:11:58 +0300

    navbar icon solved

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,14 +7,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { HashLink as Link } from 'react-router-hash-link'
 const navbar = () => {
     return (
-        <div>
-            <BrowserRouter>
-            <nav style={{position:'fixed',zIndex:'2',width:'100%' }} className=" navbar navbar-expand-lg bg-dark">
+        <BrowserRouter>
+            <nav style={{ position: 'fixed', zIndex: '2', width: '100%' }} className=" navbar navbar-expand-lg bg-dark">
                 <div className='container fluid'>
 
-                    <a className="navbar-brand" href="#"><img className='mylogo' src={logo} alt='logo..'></img></a>
+                    <a className="navbar-brand" href="/"><img className='mylogo' src={logo} alt='logo..'></img></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <FontAwesomeIcon icon={faBars} style={{color:"white"}} />
+                        <FontAwesomeIcon icon={faBars} style={{ color: "white" }} />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -25,24 +24,22 @@ const navbar = () => {
                                 <Link to='#about' smooth className="nav-link">About</Link>
                             </li>
                             <li className="nav-item">
-                            <Link to='#services' smooth className="nav-link">services</Link>
+                                <Link to='#services' smooth className="nav-link">services</Link>
                             </li>
                             <li className="nav-item">
-                            <Link to='#experience' smooth className="nav-link">Experiences</Link>
+                                <Link to='#experience' smooth className="nav-link">Experiences</Link>
                             </li>
                             <li className="nav-item">
-                            <Link to='#projects' smooth className="nav-link">Projects</Link>
+                                <Link to='#projects' smooth className="nav-link">Projects</Link>
                             </li>
                             <li className="nav-item">
-                            <Link to='#contact' smooth className="nav-link">Contacts</Link>
+                                <Link to='#contact' smooth className="nav-link">Contacts</Link>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
-            </BrowserRouter>
-        </div>
-
+        </BrowserRouter>
     )
 }
 
